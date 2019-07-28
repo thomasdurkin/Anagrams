745ae90453a5b09a1c5ea72d348dd8f84c6b56bf
tdurkin18 <durkim@rpi.edu>
2019-07-28 17:45:24 -0400

    finished splashscreen

---
+++ src/Background.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+class Background extends JLabel{
+	Image bg = new ImageIcon("../resources/background.jpg").getImage();
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
+	}
+}
--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,125 +1,95 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
-import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.io.File;
+import java.io.IOException;
 
-public class StartScreen implements ActionListener{
+public class StartScreen extends JFrame{
+	private Font font;
 	
-	JButton hostButton;
-	JButton joinButton;
-	
-	public JFrame home;
 	
 	public StartScreen() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		final int SCREEN_HEIGHT = (int) screenSize.getHeight();
 		final int SCREEN_WIDTH = (int) screenSize.getWidth();
 		
-		home = new JFrame("Anagrams");
-		JPanel buttonPanel = new JPanel();
-		
-		buttonPanel.setLayout(new GridLayout(2,1));
-		buttonPanel.setSize(125,320);
-		
-		home.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-		home.setLayout(null);
-		home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-//		BufferedImage i;
-//		ImageIcon icon;
-//		JPanel p = new JPanel();
-//		JLayeredPane layer = new JLayeredPane();
-//		try {
-//			i = ImageIO.read(new File("../images/try.jpg"));
-//			icon = new ImageIcon(i);
-//			JLabel background = new JLabel("",icon, JLabel.CENTER);
-//			p.add(background);
-//			p.setBounds(0, 0, 1920, 1080);
-//			home.add(p);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		layer.add(p,0);
-
-		ImageIcon hostImage = new ImageIcon("../images/hostButton.png");
-		hostButton = new JButton("", hostImage);
-		hostButton.setBorder(BorderFactory.createEmptyBorder());
-		hostButton.setContentAreaFilled(false);
-		hostButton.setFocusable(false);
-		hostButton.addActionListener(this);
-		hostButton.setBounds(50,150,150,160);
-		
-		
-		ImageIcon joinImage = new ImageIcon("../images/joinButton.png");
-		joinButton = new JButton("", joinImage);
-		joinButton.setIcon(joinImage);
-		joinButton.setBorder(BorderFactory.createEmptyBorder());
-		joinButton.setContentAreaFilled(false);
-		joinButton.setFocusable(false);
-		joinButton.addActionListener(this);
-		joinButton.setBounds(50,50,150,160);
-		
-
-
-		home.add(joinButton);
-		home.add(hostButton);
-		
-		home.revalidate();
-		home.setVisible(true);
-		home.setResizable(false);
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(hostButton) == true) {
-			System.out.println("hosting");
-			
-			home.remove(joinButton);
-			home.remove(hostButton);
-			
-			ImageIcon waitingImage = new ImageIcon("../images/waiting.png");
-			JLabel waiting = new JLabel(waitingImage);
-			waiting.setBounds(50,50,300,300);
-			home.add(waiting);
-			
-			home.revalidate();
-			home.repaint();
-			
-			new Host();
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, new File("../resources/Scramble.ttf"));
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			ge.registerFont(font);
 		}
-		if(e.getSource().equals(joinButton) == true) {
-			System.out.println("joining");
-			
-			home.remove(joinButton);
-			home.remove(hostButton);
-			home.revalidate();
-			home.repaint();
-			
-			ImageIcon waitingImage = new ImageIcon("../images/waiting.png");
-			JLabel waiting = new JLabel(waitingImage);
-			waiting.setBounds(50,50,300,300);
-			home.add(waiting);
-			
-			new Client();
+		catch(IOException | FontFormatException e) {
+			e.printStackTrace();
 		}
 		
+		JFrame frame = new JFrame();
+		frame.setSize(SCREEN_WIDTH ,SCREEN_HEIGHT);
+		frame.setTitle("Anagrams");
+		frame.setLayout(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel background = new Background();
+		
+	
+		
+		
+		JLabel host = new JLabel("Host");
+		host.setFont(font.deriveFont(50f));
+		host.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		host.setBounds(SCREEN_WIDTH/2 - 125, SCREEN_HEIGHT - 300, 300, 50);
+		
+		JLabel join = new JLabel("Join");
+		join.setFont(font.deriveFont(50f));
+		join.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		join.setBounds(SCREEN_WIDTH/2 - 125, SCREEN_HEIGHT - 225, 300, 50);
+		
+		host.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.remove(join);
+				frame.remove(host);
+				frame.repaint();
+				new Host();
+				JLabel waiting = new JLabel("<html>Waiting<br/>for player</html>", SwingConstants.CENTER);
+				waiting.setFont(font.deriveFont(40f));
+				waiting.setBounds(SCREEN_WIDTH/2 - 375, SCREEN_HEIGHT - 400, 800, 300);
+				frame.add(waiting);
+			}
+		});
+		
+		
+		join.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.remove(join);
+				frame.remove(host);
+				frame.repaint();
+				new Client();
+				JLabel waiting = new JLabel("<html>Waiting<br/>for host</html>", SwingConstants.CENTER);
+				waiting.setFont(font.deriveFont(40f));
+				waiting.setBounds(SCREEN_WIDTH/2 - 375, SCREEN_HEIGHT -400, 800, 300);
+				frame.add(waiting);
+			}
+		});
+		
+		background.add(host);
+		background.add(join);
+	
+		frame.setContentPane(background);
+		
+		frame.setVisible(true);
+		
 	}
+
+	
 }
